277845d65f0bacbeda38e2d0007127bae801e24e
Imovim <118859142+Imovim@users.noreply.github.com>
2023-04-05 20:27:06 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let numberDays = document.getElementById("numberDays")
+let text = document.getElementById("text")
+
+function clock(){
+  let beginData = new Date("2023-02-14")
+  let actualData = new Date()
+
+  let remainingDays = Math.trunc((actualData.getTime() - beginData.getTime()) / (1000 * 3600 *24));
+
+  numberDays.innerHTML = 100 - remainingDays
+  
+  
+}
+
+clock()
